b3b92d01a2e328c0f11845c61a221cc8ec23bbcc
skarlywarly <skarlwarly@gmail.com>
2022-03-24 20:17:23 +1300

    :lipstick:

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const GUCCI_ADDRESS = "0x572e33ffa523865791ab1c26b42a86ac244df784";
 const App = () => {
   const [id, setId] = useState("");
   const [data, setData] = useState(null);
-  const [bg, setBg] = useState("");
+  const [bg, setBg] = useState("00ff00");
 
   useEffect(() => {
     const canvas = document.getElementById("canvas");
@@ -53,7 +53,7 @@ const App = () => {
   return (
     <div className="flex flex-col items-center w-full h-full">
       <div className="flex flex-col items-center max-w-screen-sm">
-        <div className="mt-4 text-3xl">🇬uccifed</div>
+        <div className="mt-4 text-3xl font-bold">GUCCIFIED</div>
         <input
           placeholder="Enter Token Id"
           name="id"
@@ -75,11 +75,11 @@ const App = () => {
         <canvas id="canvas" width="350px" height="350px" />
         <div
           onClick={generatePreview}
-          className="px-4 py-2 mt-2 text-lg border-2"
+          className="px-4 py-2 mt-2 text-lg font-bold border-2"
         >
           Generate Preview
         </div>
-        <div className="px-4 py-2 mt-4 text-lg border-2" onClick={() => {
+        <div className="px-4 py-2 mt-4 text-lg font-bold border-2" onClick={() => {
         alert('Just right-click-save mfer')
         }}>Download</div>
       </div>
